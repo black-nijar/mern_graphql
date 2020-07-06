89551f324b469218aeca48d80acfad7dfb92f53d
Nijarudeen <nijarr2020@gmail.com>
2020-07-06 19:48:25 +0530

    comment deleted

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -79,6 +79,23 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError('Post not found');
+    },
+
+    // DELETE COMMENT
+    async deleteComment(_, { postId, commentId }, context){
+      const { username } = auth(context);
+
+      const post  = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex(comm => comm.id === commentId);
+
+        if (post.comments[commentIndex].username === username){
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else throw new AuthenticationError('Action not allowed')
+      } else throw new UserInputError('Post not found')
     }
   }
 };
