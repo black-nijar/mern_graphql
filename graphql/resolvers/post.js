24d9ad8ace6234592ff454d002b2a789dc99ce0e
Nijarudeen <nijarr2020@gmail.com>
2020-07-06 18:40:36 +0530

    posts are protected

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,11 +1,12 @@
 const Post = require('../../models/Post');
 const auth = require('../../utils/auth');
+const { AuthenticationError } = require('apollo-server');
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (error) {
         throw new Error(error);
@@ -37,6 +38,20 @@ module.exports = {
       // Save post to mongoDB
       const post = await newPost.save();
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = auth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.deleteOne();
+          return 'Post deleted successfully'
+        } else {
+          throw new AuthenticationError('Action not allowed')
+        }
+      } catch (error) {
+        throw new Error(error)
+      }
     }
   }
 };
